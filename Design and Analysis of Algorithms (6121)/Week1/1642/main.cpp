4d073b8241336d9341a30afe18b82903f7d9d15e
Ilyas Karimov <mrilyaskarimov@gmail.com>
2020-09-17 00:09:57 +0400

    W1 done

--- Design and Analysis of Algorithms (6121)/Week1/1642/main.cpp
+++ Design and Analysis of Algorithms (6121)/Week1/1642/main.cpp
@@ -10,9 +10,18 @@
 
 int main(int argc, const char * argv[]) {
  //n! is divided by n anyways. so, (n-1)! should be divisible by n. but some multiplication of dividents of factorial can make n.
-    int factorial;
+// if n is prime, then no, if
+
+    int factorial, count = 0;
     scanf("%d", &factorial);
-    for (int i = 2; i; i <=  factorial -1; i++){
-    
+    for (int i = 2; i*i <=  factorial; i++){
+        if(factorial%i == 0) count++;
+        
     }
+    if (factorial ==1) printf("%s", "YES");
+    else if (factorial ==4) printf("%s", "NO");
+    else{
+        if (count>0) printf("%s", "YES");
+        else printf("%s", "NO");
+        }
 }
