a7e02338e59d8296a5426e1b22c451d174bce1a5
Ilyas Karimov <mrilyaskarimov@gmail.com>
2020-09-16 21:34:33 +0400

    2 questions left

--- Design and Analysis of Algorithms (6121)/Week1/5329/main.cpp
+++ Design and Analysis of Algorithms (6121)/Week1/5329/main.cpp
@@ -4,12 +4,20 @@
 //
 //  Created by Ilyas Karimov on 9/10/20.
 //  Copyright © 2020 Ilyas Karimov. All rights reserved.
-//
+//PARTY
 
 #include <iostream>
+int party [501][501];
 
+int Cnk (int n, int k){
+    if (k==n || k==0) return 1;
+    if (party[n][k] != -1 ) return party[n][k];
+    return party[n][k] = ((Cnk(n-1, k - 1) + Cnk(n-1, k)))%9929;
+    
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    int n,k;
+    scanf("%d %d", &n,&k);
+    memset(party, -1, sizeof(party));
+    printf("%d", (Cnk(n, k)));
 }
--- Design and Analysis of Algorithms (6121)/Week1/9557/main.cpp
+++ Design and Analysis of Algorithms (6121)/Week1/9557/main.cpp
@@ -5,13 +5,24 @@
 //  Created by Ilyas Karimov on 9/10/20.
 //  Copyright © 2020 Ilyas Karimov. All rights reserved.
 //
+//
+
 
 #include <iostream>
 
-int bin (int n){
-    if (n == 1 ) return 1;
-    return n
+using namespace std;
+
+long long m = 1000000007;
+
+//Return (n*bb(n%m,k-1)%m
+
+long long  binsballs (long long n, long long k){
+    if (k == 0) return 1;
+    if (k%2 == 0) return (binsballs((n*n)%m, k/2)%m);
+    return (n*binsballs(n%m, k-1)%m);
 }
 int main(int argc, const char * argv[]) {
-    print
+    long long n;
+    scanf("%lld", &n);
+    printf("%lld", (n*binsballs(n-1, n-1)));
 }
