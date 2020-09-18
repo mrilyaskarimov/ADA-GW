c34383125d6914d2cac2d12199981af804ccdb98
Ilyas Karimov <mrilyaskarimov@gmail.com>
2020-09-18 11:32:31 +0400

    Week2.2 Comit

---
+++ Design and Analysis of Algorithms (6121)/Week2.2/Week2.2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Week2.2
+//
+//  Created by Ilyas Karimov on 9/18/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
