486afdb34b4b519b019685ded3272ada60ab6998
Ilyas Karimov <mrilyaskarimov@gmail.com>
2020-09-19 14:54:33 +0400

    7 ve 8 from Algo w2.1

--- Design and Analysis of Algorithms (6121)/Week2.1/6129/main.cpp
+++ Design and Analysis of Algorithms (6121)/Week2.1/6129/main.cpp
@@ -7,9 +7,62 @@
 //
 
 #include <iostream>
+#include <deque>
+#include <cstring>
+
+#define MAX 100
+using namespace std;
+
+deque <int> deq;
+char str[MAX];
+int num;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    while(scanf("%s", str) == 1){
+        if(strcmp(str, "push_back")==0){
+                    scanf("%d", &num);
+                    deq.push_back(num);
+                    puts("ok");
+        }
+        if(strcmp(str, "push_front")==0){
+            scanf("%d", &num);
+            deq.push_front(num);
+            puts("ok");
+        }
+        if(strcmp(str, "size") ==0){
+            printf("%d\n", deq.size());
+        }
+        if(strcmp(str, "clear")==0){
+            deq.clear();
+            puts("ok");
+        }
+        
+        if(strcmp(str, "exit")==0){
+            puts("bye");
+            return 0;
+        }
+        if(!deq.empty()){
+            if(strcmp(str, "pop_back")==0){
+                printf("%d\n", deq.back());
+                deq.pop_back();
+            }
+            if(strcmp(str, "pop_front")==0){
+                printf("%d\n", deq.front());
+                deq.pop_front();
+            }
+            if(strcmp(str, "back")==0){
+                printf("%d\n", deq.back());
+            }
+            if(strcmp(str, "front")==0){
+                printf("%d\n", deq.front());
+            }
+        }
+        else{
+            if(strcmp(str, "clear")!=0 && strcmp(str, "size")!=0){
+                puts("error");
+            }
+          
+        }
+            
+    }
 }
--- Design and Analysis of Algorithms (6121)/Week2.1/8355/main.cpp
+++ Design and Analysis of Algorithms (6121)/Week2.1/8355/main.cpp
@@ -7,9 +7,34 @@
 //
 
 #include <iostream>
+#include <deque>
+
+using namespace std;
+deque<int> deq;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    int count, num, num2;
+    scanf("%d", &count);
+    for(int i=0; i<count; i++){
+        scanf("%d", &num);
+        if(num == 1 || num == 2){
+            scanf("%d", &num2);
+            if(num == 1){
+                deq.push_back(num2);
+            }
+            else{
+                deq.push_front(num2);
+            }
+        }
+        if(num == 3 || num == 4){
+            if(num == 3){
+                printf("%d\n", deq.back());
+                deq.pop_back();
+            }
+            else{
+                printf("%d\n", deq.front());
+                deq.pop_front();
+            }
+        }
+    }
 }
