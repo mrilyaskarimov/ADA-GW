59040bc99d090d67aa5b9b984cc76b90ecf15f4e
Ilyas Karimov <mrilyaskarimov@gmail.com>
2020-09-19 10:33:58 +0400

    6th exercise

--- Design and Analysis of Algorithms (6121)/Week2.1/6128/main.cpp
+++ Design and Analysis of Algorithms (6121)/Week2.1/6128/main.cpp
@@ -8,7 +8,50 @@
 
 #include <iostream>
 #include <stack>
+#include <deque>
+#include <cstring>
+using namespace std;
+
+deque <int> deq;
+char str[101];
+int num;
 
 int main(int argc, const char * argv[]) {
-  
+    while( scanf("%s",str) ==1){
+        if(strcmp(str, "push_back")==0){
+            scanf("%d", &num);
+            deq.push_back(num);
+            puts("ok");
+        }
+        if(strcmp(str, "push_front")==0){
+            scanf("%d", &num);
+            deq.push_front(num);
+            puts("ok");
+        }
+        if(strcmp(str, "size") ==0){
+            printf("%d\n", deq.size());
+        }
+        if(strcmp(str, "clear")==0){
+            deq.clear();
+            puts("ok");
+        }
+        if(strcmp(str, "pop_back")==0){
+            printf("%d\n", deq.back());
+            deq.pop_back();
+        }
+        if(strcmp(str, "pop_front")==0){
+            printf("%d\n", deq.front());
+            deq.pop_front();
+        }
+        if(strcmp(str, "back")==0){
+            printf("%d\n", deq.back());
+        }
+        if(strcmp(str, "front")==0){
+            printf("%d\n", deq.front());
+        }
+        if(strcmp(str, "exit")==0){
+            puts("bye");
+            return 0;
+        }
+    }
 }
